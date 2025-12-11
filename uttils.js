022fb51f5271951be1be81fb41a0dd62a96ed1d2
Avi Fisher <avifisher82@gmail.com>
2025-12-11 09:31:55 +0200

    create input function in uttils.js file

---
+++ uttils.js
@@ -0,0 +1,17 @@
+import readline from "readline-sync"
+
+export function input(print = ""){
+
+    const input = readline.question(print)
+    return input
+}
+
+
+export function input_int(print = ""){
+
+    const input = readline.questionInt(print)
+    return input
+}
+
+
+
